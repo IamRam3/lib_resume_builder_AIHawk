f744872bc6e07fff3cd01bd628ee52692dcebc76
IamRam3 <136904304+IamRam3@users.noreply.github.com>
2024-09-02 18:44:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'langchain-openai',
         'langchain-text-splitters',
         'langsmith',
-        'openai'
+        'openai',
         'google-generativeai',
         'langchain_google_genai',
         'regex==2024.7.24',
